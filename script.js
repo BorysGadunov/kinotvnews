761109c3efb1b8d743e82ffc6a42a5c132997c2b
Borys <lichking.darkness@gmail.com>
2016-02-12 13:34:50 +0200

    add bootstap style

---
+++ script.js
@@ -0,0 +1,24 @@
+function adding(a, b){
+	insert a;
+
+	return a+b;
+}
+if (a > 10) {
+	alert "Buhahashenki"
+};
+innerHTML("quotes")
+document.write("Hello, World")
+
+class Animal{
+	var rod;
+	var legs;
+
+	function walk{
+		if (true) {return true; }
+		else return false;
+	}
+new Animal = "Garfield"
+}
+class Dog from  {
+
+}
